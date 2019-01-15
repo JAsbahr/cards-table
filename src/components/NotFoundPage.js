8052ade41d0160b489c309431bee91387cc14c5f
Johannes Asbahr <hannes-asbahr@hotmail.de>
2019-01-15 15:38:58 +0100

    page 404 changed

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -2,8 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NotFoundPage = () => (
-    <div>
-        404 - <Link to="/forpuca">Go home</Link>
+    <div className="page-header">
+        <div className="content-container">
+            <h1 className="page-header__404">Page not found. What did you expect, though?</h1>
+            <Link className="button" to="/forpuca">Go home, you're drunk</Link>
+        </div>
     </div>
 );
 
