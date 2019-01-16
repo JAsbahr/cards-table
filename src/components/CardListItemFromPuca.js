1ff5fb6e2713b41e14083785677969a762487fbc
Johannes Asbahr <hannes-asbahr@hotmail.de>
2019-01-17 00:06:31 +0100

    changed math behind percentage

--- src/components/CardListItemFromPuca.js
+++ src/components/CardListItemFromPuca.js
@@ -5,7 +5,7 @@ import numeral from "numeral"
 const CardListItemFromPuca = ({ id, description, euro, amount, sent, puca, location }) => { // Everything inside Link is clickable
     const formattedEuro = numeral(euro / 100 * 0.95).format("0,0.00")
     const percentage = Math.round(((euro * 0.95 * sent / (puca * sent)) - 1) * 100) * 2
-    const percentagePromo = Math.round(((puca * sent / (euro * 0.95) * sent) - 1) * 100)
+    const percentagePromo = Math.round(((puca * sent / ((euro * 0.95) * sent)) - 1) * 100)
     let color
     if (percentage >= 100) {
         color = "item-green"
